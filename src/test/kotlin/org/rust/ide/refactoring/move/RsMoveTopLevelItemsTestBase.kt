632d1c1a6b003d83778afebd17373b595c997fc7
Dmitry Murzin <diralik@yandex.ru>
2020-06-26 23:29:40 +0300

    T: Base class for move items refactoring

---
+++ src/test/kotlin/org/rust/ide/refactoring/move/RsMoveTopLevelItemsTestBase.kt
@@ -0,0 +1,94 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package org.rust.ide.refactoring.move
+
+import com.intellij.openapi.application.runWriteAction
+import com.intellij.openapi.editor.Document
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiDirectory
+import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.psi.util.parentOfType
+import org.intellij.lang.annotations.Language
+import org.rust.RsTestBase
+import org.rust.TestProject
+import org.rust.lang.core.psi.ext.RsItemElement
+import org.rust.lang.core.psi.ext.RsMod
+import org.rust.lang.core.psi.ext.childrenOfType
+import org.rust.openapiext.document
+import org.rust.openapiext.toPsiDirectory
+import org.rust.openapiext.toPsiFile
+import org.rust.stdext.mapToSet
+
+abstract class RsMoveTopLevelItemsTestBase : RsTestBase() {
+
+    protected fun doTest(@Language("Rust") before: String, @Language("Rust") after: String) =
+        checkByDirectory(before.trimIndent(), after.trimIndent(), ::performMove)
+
+    private fun performMove(testProject: TestProject) {
+        val sourceFile = myFixture.findFileInTempDir(testProject.fileWithCaret).toPsiFile(project)!!
+        val itemsToMove = sourceFile.getElementsAtMarker().mapToSet { it.parentOfType<RsItemElement>()!! }
+
+        val root = myFixture.findFileInTempDir(".").toPsiDirectory(project)!!
+        val targetMod = searchElementInAllFiles(root) { it.getElementAtMarker(TARGET_MARKER) }
+            ?.parentOfType<RsMod>()
+            ?: error("Please add $TARGET_MARKER marker for target mod")
+
+        RsMoveTopLevelItemsProcessor(
+            project,
+            itemsToMove,
+            targetMod,
+            searchForReferences = true
+        ).run()
+    }
+
+    private fun <T> searchElementInAllFiles(root: PsiDirectory, searcher: (PsiFile) -> T?): T? {
+        for (file in root.childrenOfType<PsiFile>()) {
+            searcher(file)?.let { return it }
+        }
+        for (directory in root.childrenOfType<PsiDirectory>()) {
+            searchElementInAllFiles(directory, searcher)?.let { return it }
+        }
+        return null
+    }
+
+    companion object {
+        private const val TARGET_MARKER = "/*target*/"
+    }
+}
+
+private fun PsiFile.getElementAtMarker(marker: String = "<caret>"): PsiElement? =
+    getElementsAtMarker(marker).singleOrNull()
+
+private fun PsiFile.getElementsAtMarker(marker: String = "<caret>"): List<PsiElement> =
+    extractMultipleMarkerOffsets(project, marker).map { findElementAt(it)!! }
+
+private fun PsiFile.extractMultipleMarkerOffsets(project: Project, marker: String): List<Int> =
+    virtualFile.document!!.extractMultipleMarkerOffsets(project, marker)
+
+private fun Document.extractMultipleMarkerOffsets(project: Project, marker: String): List<Int> {
+    if (!text.contains(marker)) return emptyList()
+
+    val offsets = mutableListOf<Int>()
+    runWriteAction {
+        val text = StringBuilder(text)
+        while (true) {
+            val offset = text.indexOf(marker)
+            if (offset >= 0) {
+                text.delete(offset, offset + marker.length)
+                offsets += offset
+            } else {
+                break
+            }
+        }
+        setText(text.toString())
+    }
+
+    PsiDocumentManager.getInstance(project).commitAllDocuments()
+    PsiDocumentManager.getInstance(project).doPostponedOperationsAndUnblockDocument(this)
+    return offsets
+}
